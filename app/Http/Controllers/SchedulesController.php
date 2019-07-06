fbb1ab5423e7fe49a7a2af7f76f33056107d42bb
Zuhdan Nur Ihsan <pczuhdan33@gmail.com>
2019-07-06 11:45:34 +0700

    fix composer

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -172,7 +172,7 @@ class SchedulesController extends Controller
     }
 
     public function studentSchedule(Request $request) {
-        $data= \App\Schedule::where('requester_id', $request->id_user)->get();
+        $data = \App\Schedule::where('requester_id', $request->id_user)->get();
             // $data['result']['user'] = \App\User::where('id', $data['result']->consultant_id)->get();
 
             return [
