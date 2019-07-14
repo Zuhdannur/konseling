5450e387868d0c7908ba1654020e6e34d65683f5
AldiSatrioAji <aldis85@yahoo.com>
2019-07-14 18:20:45 +0700

    test upcoming

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -134,7 +134,7 @@ class SchedulesController extends Controller
                 });
                 $query->where('type_schedule', $id);
                 $query->where('status', $stat);
-                if($upcoming) $query->where('time', '>', Carbon::now());
+                if(!$upcoming) $query->where('time', '>', Carbon::now());
                 else $query->where('time', '<', Carbon::now());
             })->with('request')->with('consultant')->orderBy('id','desc');
 
