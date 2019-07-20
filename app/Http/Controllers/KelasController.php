c788af3fd177318ca67b75fa3629a862c232801c
Aldi Satrio Aji <aldis85@yahoo.com>
2019-07-20 19:42:03 +0700

    asd

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -8,10 +8,7 @@ class KelasController extends Controller
     public function all()
     {
         $data = \App\Kelas::all();
-        return Response::json([
-            "message" => "success",
-            "result" => $data
-        ], 200);
+        return Response::json($data, 200);
     }
 
     public function add(Request $request)
