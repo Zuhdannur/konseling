a87c403e9063fb9bd1129b47cc4b6f970823b68a
Aldi Satrio Aji <aldis85@yahoo.com>
2019-07-20 15:24:28 +0700

    update

--- app/Http/Controllers/MastersController.php
+++ app/Http/Controllers/MastersController.php
@@ -6,24 +6,23 @@ use Illuminate\Support\Facades\Validator;
 
 class MastersController extends Controller
 {
-
     public function getListSchool()
     {
         $data = \App\School::all();
         return Response::json([
             "message" => "success",
             "result" => $data
-        ],200);
+        ], 200);
     }
 
-    public function getListClass($id)
-    {
-        $data = \App\Kelas::where('id_school', $id)->get();
-        return Response::json([
-            "message" => 'success',
-            "result" => $data
-        ],200);
-    }
+    // public function getListClass($id)
+    // {
+    //     $data = \App\Kelas::where('id_school', $id)->get();
+    //     return Response::json([
+    //         "message" => 'success',
+    //         "result" => $data
+    //     ], 200);
+    // }
 
     public function storeSchool(Request $request)
     {
@@ -32,7 +31,6 @@ class MastersController extends Controller
             'address' => 'required'
         ]);
         if ($validator) {
-
             $insert = new \App\School;
             $insert->school_name = $request->school_name;
             $insert->address = $request->address;
@@ -40,11 +38,11 @@ class MastersController extends Controller
             if ($insert) {
                 return Response::json([
                     "message" => "success"
-                ],200);
+                ], 200);
             } else {
                 return Response::json([
                     "message" => "failed"
-                ],201);
+                ], 201);
             }
         } else {
             return [
@@ -56,42 +54,14 @@ class MastersController extends Controller
     public function destroySchool($id)
     {
         $delete = \App\School::find($id)->delete();
-        if($delete){
+        if ($delete) {
             return Response::json([
                 "message" => 'success'
-            ],200);
+            ], 200);
         } else {
             return Response::json([
                 "message" => 'failed'
-            ],201);
-        }
-    }
-
-    public function storeClass(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'id_school' => 'required',
-            'class_name' => 'required'
-        ]);
-        if ($validator) {
-
-            $insert = new \App\Kelas;
-            $insert->id_school = $request->id_school;
-            $insert->class_name = $request->class_name;
-            $insert->save();
-            if ($insert) {
-                return Response::json([
-                    "message" => "success"
-                ],200);
-            } else {
-                return Response::json([
-                    "message" => "failed"
-                ],201);
-            }
-        } else {
-            return [
-                "message" => $validator->errors()
-            ];
+            ], 201);
         }
     }
 }
