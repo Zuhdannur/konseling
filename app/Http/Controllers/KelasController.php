6d7fb2c748bb8f5a7771f59364052898ebf5b288
Aldi Satrio Aji <aldis85@yahoo.com>
2019-07-20 15:09:10 +0700

    asd

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -4,40 +4,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
 
-class KelasController extends Controller {
-
-    public function all() {
+class KelasController extends Controller
+{
+    public function all()
+    {
         $data = \App\Kelas::all();
         return Response::json([
             "message" => "success",
             "result" => $data
-        ],200);
+        ], 200);
     }
 
-    public function add(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'id_school' => 'required',
-            'class_name' => 'required'
-        ]);
-        if ($validator) {
-
-            $insert = new \App\Kelas;
-            $insert->id_school = $request->id_school;
-            $insert->class_name = $request->class_name;
-            $insert->save();
-            if ($insert) {
-                return Response::json([
-                    "message" => "success"
-                ],200);
-            } else {
-                return Response::json([
-                    "message" => "failed"
-                ],201);
-            }
+    public function add(Request $request)
+    {
+        $insert = new \App\Kelas;
+        $insert->nama_kelas = $request->nama_kelas;
+        $insert->save();
+        if ($insert) {
+            return Response::json([
+                "message" => "successfuly"
+            ], 200);
         } else {
-            return [
-                "message" => $validator->errors()
-            ];
+            return Response::json([
+                "message" => "failed"
+            ], 201);
         }
     }
 }
