3f51a37e4856492c218ecaf1b81d7b931dc8628e
AldiSatrioAji <aldis85@yahoo.com>
2019-07-22 22:02:37 +0700

    ASd

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ $router->group(['prefix'=>'v1/api'], function () use ($router) {
 
     //For Develpment Purposes
     $router->get('user', 'UsersController@all');
+    $router->get('user/{id}', 'UsersController@get');
 
     $router->group(['middleware' => 'auth'], function () use ($router) {
         
@@ -109,7 +110,6 @@ $router->group(['prefix'=>'v1/api'], function () use ($router) {
          * Routes for resource user
          */
         // $router->get('user/{id}', 'UsersController@get');
-        $router->get('user/{id}', 'UsersController@get');
         $router->put('user', 'UsersController@put');
         $router->delete('user/{id}', 'UsersController@remove');
             
