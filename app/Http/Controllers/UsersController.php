f53d9e4cfa588b6551168e66cd68cf2cfbf53963
AldiSatrioAji <aldis85@yahoo.com>
2019-07-23 08:02:18 +0700

    asd

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -136,7 +136,9 @@ class UsersController extends Controller
         // $query = \App\User::where('role','guru')->whereHas('detail', function($q) {
         //     $q->with('sekolah')->where('id_sekolah', Auth::user()->detail->id_sekolah);
         // })->get();
-        $query = \App\User::with('detail', 'detail.sekolah')->where('role','guru')->where('id_sekolah', Auth::user()->detail->id_sekolah)->get();
+        $query = \App\User::where('role','guru')->with('detail', 'detail.sekolah', function ($q){
+            $q->where('id_sekolah', Auth::user()->detail->id_sekolah);
+        })->get();
         return Response::json($query, 200);
     }
 
