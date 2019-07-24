29cae404fa2e641fe0cb389f667f84d71631c038
AldiSatrioAji <aldis85@yahoo.com>
2019-07-24 17:01:54 +0700

    new

--- app/Http/Controllers/NotifikasisController.php
+++ app/Http/Controllers/NotifikasisController.php
@@ -8,21 +8,22 @@ class NotifikasisController extends Controller {
 
     public function all(Request $filters) {
         $app = new \App\Notification;
-        $app = $app->where('id_user', Auth::user()->id)->get();
+        $app = $app->where('id_user', Auth::user()->id);
 
         $limit = $filters->limit;
         if (empty($filters->page)) $skip = 0;
         else $skip = $limit * $filters->page;
+
         $data = $app
             ->skip($skip)
             ->take($limit);
 
-            return Response::json($data, 200);
-        }
+        return Response::json($data, 200);
+    }
 
     public function notifikasiCount(Request $filters) {
         $app = new \App\Notification;
-        $app = $app->where('id_user', Auth::user()->id)->get();
+        $app = $app->where('id_user', Auth::user()->id);
 
         $limit = $filters->limit;
 
