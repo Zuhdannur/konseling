502694adf93f26eb528afe162117259f4187b36a
AldiSatrioAji <aldis85@yahoo.com>
2019-07-26 09:30:05 +0700

    schedule cancel

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -594,7 +594,7 @@ class SchedulesController extends Controller
             }
         } else {
             //Role Siswa
-            $schedule = \App\Schedule::where('id', $request->id)->where('requester_id', $request->requester_id)->where('status', 1)->first();
+            $schedule = \App\Schedule::where('id', $request->id)->where('requester_id', $request->requester_id)->where('status', $request->status)->first();
             if($schedule) {
 
                 // $senderName = \App\User::where('id', $schedule['requester_id'])->first()->name;
