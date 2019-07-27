2afc59aee9fd48ed88deae4cfe70d45076e47101
Aldi Satrio Aji <aldis85@yahoo.com>
2019-07-27 12:10:49 +0700

    fix request

--- app/Http/Controllers/RiwayatsController.php
+++ app/Http/Controllers/RiwayatsController.php
@@ -18,9 +18,8 @@ class RiwayatsController extends Controller
         $datas = \App\Riwayat::where('user_id', Auth::user()->id);
         $datas->with('schedule')->with('user');
 
-        if($request->has('orderBy'))
-        {
-            $datas = $request->where($request->orderBy, 'desc');
+        if ($request->has('orderBy')) {
+            $datas = $datas->where($request->orderBy, 'desc');
         }
 
         $data = $datas
