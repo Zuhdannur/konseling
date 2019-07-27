b96205d515b25c98627582b189f47b57e147a344
Aldi Satrio Aji <aldis85@yahoo.com>
2019-07-27 18:51:24 +0700

    asd

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -354,7 +354,6 @@ class SchedulesController extends Controller
         } else {
             //Role Siswa
             $schedule = \App\Schedule::where('id', $id)->where('status', $status)->first();
-            dd($schedule == null);
             if($schedule != null) {
 
                 // $senderName = \App\User::where('id', $schedule['requester_id'])->first()->name;
@@ -391,7 +390,7 @@ class SchedulesController extends Controller
                     return \Illuminate\Support\Facades\Response::json(["message" => "Pengajuan gagal dibatalkan."], 201);
                 }
             } else {
-                return \Illuminate\Support\Facades\Response::json(["message" => "Pengajuan telah diterimas oleh guru."], 201);
+                return \Illuminate\Support\Facades\Response::json(["message" => "Pengajuan telah diterima oleh guru."], 201);
             }
         }
         
