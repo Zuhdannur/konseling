68fe461c5c0afc3a7b38cb7aa802a560127481f8
Aldi Satrio Aji <aldis85@yahoo.com>
2019-07-27 10:02:39 +0700

    history

--- app/Http/Controllers/RiwayatsController.php
+++ app/Http/Controllers/RiwayatsController.php
@@ -2,13 +2,43 @@
 
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Lumen\Http\Request;
 
 class RiwayatsController extends Controller
 {
-    public function all()
+    public function all(Request $filters)
     {
-        $data = \App\Riwayat::where('user_id', Auth::user()->id)->with('schedule')->with('user')->get();
-        return Response::json($data, 200);
+        $data = \App\Riwayat::where('user_id', Auth::user()->id);
+        $data->with('schedule')->with('user')->get();
+
+        $limit = $filters->limit;
+        if (empty($filters->page)) {
+            $skip = 0;
+        } else {
+            $skip = $limit * $filters->page;
+        }
+        $datas = $data
+            ->skip($skip)
+            ->take($limit);
+
+        return Response::json($datas, 200);
+    }
+
+    public function count(Request $filters)
+    {
+        $data = \App\Riwayat::where('user_id', Auth::user()->id);
+
+        $limit = $filters->limit;
+        if (empty($filters->page)) {
+            $skip = 0;
+        } else {
+            $skip = $limit * $filters->page;
+        }
+        $count = $data
+            ->paginate($skip)
+            ->lastPage($limit);
+
+        return Response::json(['total_page' => $count], 200);
     }
 
     public function get($id)
