e2e6f2cc8fe504b8c5dc665f6b04801ecc7dfaf1
Aldi Satrio Aji <aldis85@yahoo.com>
2019-07-27 15:18:59 +0700

    a

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -142,7 +142,7 @@ class SchedulesController extends Controller
 
     public function finish(Request $request) {
         if (Auth::user()->role == 'guru') {
-            $schedule = \App\Schedule::where('id', $request->id)->where('ended', 0)->first();
+            $schedule = \App\Schedule::where('id', $request->id)->first();
             dd($schedule);
             if($schedule) {
                 $schedule = $schedule->update([
