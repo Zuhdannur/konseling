213ec6705b4598adb6c2d3b534b42ef7fc2c22f6
Aldi Satrio Aji <aldis85@yahoo.com>
2019-08-01 21:40:28 +0700

    add

--- app/Http/Controllers/RiwayatsController.php
+++ app/Http/Controllers/RiwayatsController.php
@@ -14,7 +14,7 @@ class RiwayatsController extends Controller
             $q->whereHas('detail', function ($query) use ($user) {
                 $query->where('id_sekolah', $user->id_sekolah);
             });
-        })->with('user')->with('user.detail')->with('schedule.consultant')->with('schedule.requester')->orderBy('id', 'desc');
+        })->with('user')->with('user.detail')->with('schedule.consultant')->with('schedule.request')->orderBy('id', 'desc');
 
         if ($request->has('isToday')) {
             if ($request->isToday == 'true') {
