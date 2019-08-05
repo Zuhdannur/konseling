e6251acf49264eac3a198e2c2023d826ea1347fe
Aldi Satrio Aji <aldis85@yahoo.com>
2019-08-05 08:01:42 +0700

    test

--- app/Http/Controllers/CatatanKonselingsController.php
+++ app/Http/Controllers/CatatanKonselingsController.php
@@ -1,5 +1,12 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Response;
+
 class CatatanKonselingsController extends Controller
 {
+    public function all(Request $request)
+    {
+        $catatan = \App\CatatanKonseling::all();
+        return Response::json($catatan, 200);
+    }
 }
