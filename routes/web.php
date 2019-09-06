0a961117e459cc54ee3fc75fa5ee2ff3427cb22c
Aldi Satrio Aji <aldis85@yahoo.com>
2019-09-06 23:13:32 +0700

    plz

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,7 @@ $router->group(['prefix'=>'v1/api'], function () use ($router) {
          * Routes for resource schedule
          */
         $router->post('schedule', 'SchedulesController@add');
-        $router->get('schedule', 'SchedulesController@all');
+        $router->get('schedule', 'SchedulesController@all')->middleware('paging');
         $router->get('schedule/{id}', 'SchedulesController@get');
         $router->put('schedule', 'SchedulesController@put');
         $router->delete('schedule/{id}', 'SchedulesController@remove');
