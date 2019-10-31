43b136b8b4b93d754250c5f9447b1138b1b30594
Aldi Satrio Aji <aldis85@yahoo.com>
2019-10-31 21:25:01 +0700

    get feed

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ $router->group(['prefix'=>'v1/api'], function () use ($router) {
                 $router->put('sekolah', 'SekolahController@put');
                 $router->get('sekolah/count', 'SekolahController@count');
                 $router->get('user/admin/count', 'UserController@adminCount');
-                $router->get('user/recent-activity', 'UserController@recentActivity');
+                $router->get('user/recent', 'UserController@recentActivity');
             });
 
         });
