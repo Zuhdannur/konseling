4ac0589a95a8d96d6fac4c809fc1c921a3ee07ea
Aldi Satrio Aji <aldis85@yahoo.com>
2019-11-01 13:59:11 +0700

    fix

--- database/migrations/2019_07_19_201812_create_tbl_diary.php
+++ database/migrations/2019_07_19_201812_create_tbl_diary.php
@@ -14,9 +14,9 @@ class CreateTblDiary extends Migration
     public function up()
     {
         Schema::create('diary', function (Blueprint $table) {
-            $table->unsignedBigInteger('id');
+            $table->bigIncrements('id');
             $table->integer('user_id')->unsigned()->nullable();
-            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('set null');
             $table->string('title', 200);
             $table->text('body');
             $table->date('tgl');
